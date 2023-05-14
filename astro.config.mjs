b3e7d41391a131b89c74ea3d00b37601ea89fc4b
Weston Nelson <dogooddowell@protonmail.com>
2023-05-14 22:49:13 +0000

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,22 @@ import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx({
+    // `syntaxHighlight` inherited from Markdown
+
+    // `gfm` overridden to `false`
+    gfm: false,
+    // Markdown config now ignored
+    extendMarkdownConfig: false
+    // No `remarkPlugins` applied
+  }), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
